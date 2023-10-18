991442fe9652e87cddcdb74117ae7b880828a991
jay <jayly.2506@gmail.com>
2023-10-18 10:02:43 -0700

    lab4

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 def main():
 
     original_str = 'Python Programming'
-    ##################################################
-    # Comlete your code here
-    ##################################################
-
+    # secondP= original_str.find('P',1)
+    # sub1 = original_str[secondP:]
+    # sub2 = original_str[:secondP-1]
+    
+    sub1 = original_str [7:]
+    sub2 = original_str [:6]
+   
     print(sub2)
     print(sub1)
+    merged_str = sub1 + ' ' + sub2 
     print(merged_str)
 
-    #########################################
-    # Do not delete the return statement
+    
+    
     return sub1, sub2, merged_str
 
 
